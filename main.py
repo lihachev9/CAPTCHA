a6f9c7179a2a6cfef0089c02a36d34a0fe673459
lihachev9 <andreyyy.lihachev@gmail.com>
2023-05-11 22:12:58 +0300

    fix SAMModel

--- main.py
+++ main.py
@@ -11,36 +11,25 @@ validation_dataset = tf.data.experimental.load('validation_dataset')
 
 
 class SAMModel(tf.keras.Model):
-    def __init__(self, model, rho=0.05):
-        """
-        p, q = 2 for optimal results as suggested in the paper
-        (Section 2)
-        """
-        super(SAMModel, self).__init__()
-        self.model = model
-        self.rho = rho
 
     def train_step(self, data):
-        x, y = data
+        (images, labels) = data
         e_ws = []
         with tf.GradientTape() as tape:
-            predictions = self.model(x)
-            loss = self.compiled_loss(y, predictions)
-        self.compiled_metrics.update_state(y, predictions)
-        trainable_params = self.model.trainable_variables
+            predictions = self(images)
+            loss = self.compiled_loss(labels, predictions)
+        trainable_params = self.trainable_variables
         gradients = tape.gradient(loss, trainable_params)
         grad_norm = self._grad_norm(gradients)
-        scale = self.rho / (grad_norm + 1e-12)
+        scale = 0.05 / (grad_norm + 1e-12)
 
+        with tf.GradientTape() as tape:
+            predictions = self(images)
+            loss = self.compiled_loss(labels, predictions)    
         for (grad, param) in zip(gradients, trainable_params):
             e_w = grad * scale
             param.assign_add(e_w)
             e_ws.append(e_w)
-
-        with tf.GradientTape() as tape:
-            predictions = self.model(x)
-            loss = self.compiled_loss(y, predictions)    
-
         sam_gradients = tape.gradient(loss, trainable_params)
         for (param, e_w) in zip(trainable_params, e_ws):
             param.assign_sub(e_w)
@@ -48,16 +37,14 @@ class SAMModel(tf.keras.Model):
         self.optimizer.apply_gradients(
             zip(sam_gradients, trainable_params))
 
+        self.compiled_metrics.update_state(labels, predictions)
         return {m.name: m.result() for m in self.metrics}
 
     def test_step(self, data):
-        x, y = data
-        predictions = self.model(x, training=False)
-        self.compiled_metrics.update_state(y, predictions)
+        (images, labels) = data
+        predictions = self(images, training=False)
+        self.compiled_metrics.update_state(labels, predictions)
         return {m.name: m.result() for m in self.metrics}
-
-    def call(self, inputs):
-        return self.model(inputs)
 
     def _grad_norm(self, gradients):
         norm = tf.norm(
