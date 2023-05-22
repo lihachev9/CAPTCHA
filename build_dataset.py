9cd592ede51e81d402169dc4342ad0d5e93fb3af
lihachev9 <andreyyy.lihachev@gmail.com>
2023-05-22 22:14:03 +0300

    import batch_size from config

--- build_dataset.py
+++ build_dataset.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
-from config import img_width, img_height
+from config import img_width, img_height, batch_size
 
 
 data_dir = Path("./captcha_images_v2/")
